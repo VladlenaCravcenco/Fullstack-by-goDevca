6ae2746a9e82a4d0c5729418241e7d0c9664be89
Vladlena <cravcenco97@inbox.ru>
2025-07-06 23:03:36 +0300

    Refactor blog section to use SCSS and update layout
    Replaced the CSS file with an SCSS file for the blog section and updated the component to use the new styles. Enhanced the blog section layout with a grid, filter, and post cards, and improved responsiveness.

--- qwik-app/src/components/blog/blog-section.tsx
+++ qwik-app/src/components/blog/blog-section.tsx
@@ -1,8 +1,39 @@
 import {component$} from '@builder.io/qwik'
 import type {DocumentHead} from '@builder.io/qwik-city'
+import './blog-section.scss';
 
 export default component$ (()=>{
-    return <h1>Блог</h1>;
+    return (
+        <section class="blog">
+      <div class="blog__grid container">
+
+        <div class="blog__text">
+          <h2>Dev-блог</h2>
+          <p>
+            Здесь я делюсь личным опытом работы с инструментами вроде Figma, Sanity, Qwik, React, Blender, 
+            а также мыслями про дизайн, тренды, фичи и подходы, которые реально помогают в работе.
+          </p>
+        </div>
+
+        <div class="blog__post-card">Product design</div>
+        <div class="blog__post-card">UI/UX design</div>
+
+        <div class="blog__filter">
+          <ul>
+            <li class="active">🎨 Дизайн</li>
+            <li>Фреймворки</li>
+            <li>Софты</li>
+            <li>ИИ-шки</li>
+          </ul>
+          <button class="blog__all-posts-btn">Перейти к постам</button>
+        </div>
+
+        <div class="blog__post-card">Product design</div>
+        <div class="blog__post-card">UI/UX design</div>
+
+      </div>
+    </section>
+    )
 });
 
 export const head: DocumentHead = {
