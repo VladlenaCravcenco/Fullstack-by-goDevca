5ef4fe69ece21e5cba37309fb8549b7f9d9abd7e
Vladlena <cravcenco97@inbox.ru>
2025-07-11 18:35:54 +0300

    icons

--- qwik-app/src/components/header/header.tsx
+++ qwik-app/src/components/header/header.tsx
@@ -7,7 +7,7 @@ import { GlassEffect } from '~/components/ui/GlassEffect';
 export default component$(() => {
 
     const menuOpen = useSignal(false);
-    
+
     return (
         <header class="header">
             <div class="container">
@@ -30,7 +30,10 @@ export default component$(() => {
 
                 <div class="header__right">
                     <div class="lang-switch">
-                        <span class="icon">🌐</span> ru
+                        <span class="icon"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13.75 7.5C13.75 10.9518 10.9518 13.75 7.5 13.75M13.75 7.5C13.75 4.04822 10.9518 1.25 7.5 1.25M13.75 7.5H1.25M7.5 13.75C4.04822 13.75 1.25 10.9518 1.25 7.5M7.5 13.75C9.0633 12.0385 9.95172 9.81748 10 7.5C9.95172 5.18252 9.0633 2.96147 7.5 1.25M7.5 13.75C5.9367 12.0385 5.04828 9.81748 5 7.5C5.04828 5.18252 5.9367 2.96147 7.5 1.25M1.25 7.5C1.25 4.04822 4.04822 1.25 7.5 1.25" stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        </span> ru
                     </div>
                     <GlassEffect class="brief-btn">
                         Заполнить бриф
@@ -41,7 +44,12 @@ export default component$(() => {
                         class="burger"
                         onClick$={() => (menuOpen.value = !menuOpen.value)}
                     >
-                        ☰
+                        <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect y="0.5" width="12.5" height="2" rx="1" fill="#18181B" />
+                            <rect y="5" width="12.5" height="2" rx="1" fill="#18181B" />
+                            <rect y="9.5" width="12.5" height="2" rx="1" fill="#18181B" />
+                        </svg>
+
                     </button>
                 </div>
             </div>
