0a8a6c685dc5e41750acf18d23bf28cb0ccc97b5
Vladlena <cravcenco97@inbox.ru>
2025-10-24 21:44:12 +0300

    Update layout.tsx

--- qwik-app/src/routes/layout.tsx
+++ qwik-app/src/routes/layout.tsx
@@ -1,6 +1,5 @@
 import { component$, Slot } from '@builder.io/qwik';
-import Header from '~/components/header/header';   // ← замени на свой компонент хедера
-import Footer from '~/components/footer/footer';         // ← замени на свой компонент футера
+     // ← замени на свой компонент футера
 import '~/global.css';                                   // если есть глобальные стили
 
 export default component$(() => {
