8e67e4fa1dce2e7b750df4946149f32a599144f6
Vladlena <cravcenco97@inbox.ru>
2025-10-26 19:30:26 +0200

    Update layout.tsx

--- qwik-app/src/routes/layout.tsx
+++ qwik-app/src/routes/layout.tsx
@@ -1,9 +1,21 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useVisibleTask$ } from '@builder.io/qwik';
+import { useLocation } from '@builder.io/qwik-city';
 import Header from '~/components/header/header';   // ← замени на свой компонент хедера
 import Footer from '~/components/footer/footer';         // ← замени на свой компонент футера
 import '~/global.css';                                   // если есть глобальные стили
 
 export default component$(() => {
+  const loc = useLocation();
+
+  // плавный скролл при каждом переходе
+  useVisibleTask$(({ track }) => {
+    track(() => loc.url.pathname); // следим за изменением маршрута
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  });
   return (
     <>
       <Header />
